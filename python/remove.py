37b52b423e8ade2f4928df3dc225c284fac26c47
caleb-james-smith <caleb_smith2@baylor.edu>
2021-07-07 12:47:23 -0500

    Script to remove root files

---
+++ python/remove.py
@@ -0,0 +1,44 @@
+# remove.py
+import argparse
+import os
+import re
+
+# Remove root files in directory according to requirements
+# WARNING: removes files
+
+def main():
+    # options
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--input_dir",  "-i", default="",   help="input directory containing root files to remove"     )
+    parser.add_argument("--min_number", "-m", default=1e10, help="remove all files with number larger than min_number" )
+    parser.add_argument("--execute",    "-e", default = False, action = "store_true", help="execute file removal"      )
+
+    options    = parser.parse_args()
+    input_dir  = options.input_dir
+    min_number = int(options.min_number)
+    execute    = options.execute
+
+    if not os.path.exists(input_dir):
+        print("ERROR: The input directory \"{0}\" does not exist.".format(input_dir))
+        return
+
+    file_list = os.listdir(input_dir)
+    for f in file_list:
+        # skip non root files:
+        if not ".root" in f:
+            continue
+        nums = re.findall(r'\d+', f)
+        if len(nums) != 1:
+            print("ERROR: skipping file {0}, multiple nubmers found: {1}".format(f, nums))
+            continue
+        n = int(nums[0])
+        if n > min_number:
+            path = "{0}/{1}".format(input_dir, f)
+            #print(f)
+            print(path)
+            if execute:
+                os.remove(path)
+
+if __name__ == "__main__":
+    main()
+
